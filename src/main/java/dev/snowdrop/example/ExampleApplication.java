a47d353f01b47752030e2b4eec1ca7b3fed7c23f
André Luiz de Gusmão <andredgusmao@gmail.com>
2024-07-17 17:46:09 -0300

    Complete refactoring 3

--- src/main/java/dev/snowdrop/example/ExampleApplication.java
+++ src/main/java/dev/snowdrop/example/ExampleApplication.java
@@ -15,10 +15,9 @@
  */
 package dev.snowdrop.example;
 
-import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ExampleApplication {
@@ -26,9 +25,5 @@ public class ExampleApplication {
     public static void main(String[] args) {
         SpringApplication.run(ExampleApplication.class, args);
     }
-
-    @Bean
-    public JacksonJsonProvider jsonProvider() {
-        return new JacksonJsonProvider();
-    }
 }
+
